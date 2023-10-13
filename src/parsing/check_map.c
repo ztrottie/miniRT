03e8256b8a57a609ee384f58716984d643ac7b36
selfix123 <lezach33@hotmail.com>
2023-10-13 15:36:34 -0400

    nothing done today

--- src/parsing/check_map.c
+++ src/parsing/check_map.c
@@ -7,3 +7,18 @@ char	**split_line(t_data *data)
 	map = split_get_next_line(data->fd, ' ');
 	return (map);
 }
+
+int	line_len(char **splitted)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+int	check_map(t_data *data)
+{
+
+}
