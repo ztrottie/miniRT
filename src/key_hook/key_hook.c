f5f0776755171e91e2377be1dd3033fa1d60fab6
Zackary Trottier <ztrottie@c2r5p7.42quebec.com>
2023-11-16 14:16:47 -0500

    yessir

--- src/key_hook/key_hook.c
+++ src/key_hook/key_hook.c
@@ -8,19 +8,19 @@ void	movement_handler(mlx_key_data_t keydata, void *param)
 	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
 	{
 		if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-			data->cam_dir.z += 1;
+			data->pitch += 1;
 		else if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-			data->cam_dir.z -= 1;
+			data->pitch -= 1;
 		else if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
-			data->cam_dir.x -= 1;
+			data->cam.x -= 1;
 		else if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
-			data->cam_dir.x += 1;
+			data->cam.x += 1;
 		else if (keydata.key == MLX_KEY_E)
-			data->cam_dir.y += 1;
+			data->cam.y += 1;
 		else if (keydata.key == MLX_KEY_Q)
-			data->cam_dir.y -= 1;
+			data->cam.y -= 1;
 		if (keydata.key == MLX_KEY_ESCAPE)
 			mlx_close_window(data->mlx);
-		//init_viewport();
+		ray_loop(data);
 	}
 }
